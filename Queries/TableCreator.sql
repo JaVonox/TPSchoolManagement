01d7be516b7a04b9a4334d4deb2d6de2a0aef2dd
Jamie Gorman <JaVonox@users.noreply.github.com>
2021-03-09 14:21:11 +0000

    Fixed bracket + commenting issues

--- Queries/TableCreator.sql
+++ Queries/TableCreator.sql
@@ -122,16 +122,16 @@ create table Person
   Role varchar(20),
   Start_Date Date,
   Sex varchar(10),
-  Address varchar(100), -- Address need more details, could be its own table
-  Phone_Number int, -- Person could have more than one number
+  Address varchar(100), /* Address need more details, could be its own table */
+  Phone_Number int, /* Person could have more than one number */
   Medical_Information varchar(100), 
-  -- This could be its own table, Medical_Information could be more detailed and could go beyond 100 characters
-  Comments varchar(100), -- Comments could be recorded for reference, may need its own table.
+  /* This could be its own table, Medical_Information could be more detailed and could go beyond 100 characters */
+  Comments varchar(100), /* Comments could be recorded for reference, may need its own table. */
   primary key(Person_ID));
 
 create table Credit
 ( Person_ID int,
-  Balance int, -- Type may be changed
+  Balance int, /* Type may be changed */
   Last_Top_Up Date,
   primary key(Person_ID),
   foreign key(Person_ID) references Person(Person_ID));
@@ -139,7 +139,7 @@ create table Credit
 create table Transactions
 ( Transaction_ID int,
   Person_ID int,
-  Transaction_Value int, -- Type may be changed
+  Transaction_Value int, /* Type may be changed */
   Date_Of_Transaction Date,
   Primary key(Transaction_ID),
   foreign key(Person_ID) references Credit(Person_ID));
@@ -148,13 +148,13 @@ create table Staff
 ( Person_ID int,
   Salary int,
   Function varchar(20),
-  Education_and_Qualification varchar(100), -- Would need its own table for more details
-  Work_Experience varchar(100), -- Could beyond 100 characters and may not be needed here
+  Education_and_Qualification varchar(100), /* Would need its own table for more details */
+  Work_Experience varchar(100), /* Could beyond 100 characters and may not be needed here */
   National_Insurance varchar(8),
   Tax_Code varchar(10),
-  Bank_Details varchar(100), -- More details
-  Department_ID int, -- This might have to be removed
+  Bank_Details varchar(100), /* More details */
+  Department_ID int, /* This might have to be removed */
   primary key(Person_ID),
-  foreign key(Person_ID) references Person(Person_ID)
-  foreign key(Department_ID) references Department(Department_ID))
+  foreign key(Person_ID) references Person(Person_ID),
+  foreign key(Department_ID) references Department(Department_ID)
   );
