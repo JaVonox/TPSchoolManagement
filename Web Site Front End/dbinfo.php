3df7d7df59c24dd9e0977f794f35b285611a7909
Mubashar-dot <59213610+Mubashar-dot@users.noreply.github.com>
2021-04-01 15:05:54 +0100

    Add files via upload

--- Web Site Front End/dbinfo.php
+++ Web Site Front End/dbinfo.php
@@ -3,5 +3,25 @@ $dbserver = "localhost";
 $dbusername = "root";
 $dbpassword = "";
 $dbdatabase = "teamproject";
+
+function getdbserver()
+{
+	return $GLOBALS['dbserver'];
+}
+
+function getdbusername()
+{
+	return $GLOBALS['dbusername'];
+}
+
+function getdbpassword()
+{
+	return $GLOBALS['dbpassword'];
+}
+
+function getdbdatabase()
+{
+	return $GLOBALS['dbdatabase'];
+}
 ?>
 
